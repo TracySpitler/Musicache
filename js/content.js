7ac1ef41845bfef4f22ff0c8c674c8e2e2e87fa1
Tracy Spitler <tgspitler@student.fullsail.edu>
2018-03-11 23:50:30 -0400

    Updated content.js (still not working but closer than before.)

--- js/content.js
+++ js/content.js
@@ -1,40 +1,46 @@
-//create XMLHttpRequest object
-//var request = new XMLHttpRequest();
-//var key = "99774229f0f8cf05d6ec0176a0109170";
+// JS Content
 
+//var key = "99774229f0f8cf05d6ec0176a0109170";
 //create XMLHttpRequest object
 var xhr = new XMLHttpRequest();
 
-//when response has loaded
-xhr.onload = function() {
-  //create variable to store JSON data
-  var responseObject = JSON.parse(xhr.responseText);
+// Listen for a form submission
+submit.addEventListener('submit', function(e){
+  // prevent default
+  e.preventDefault();
 
   var albumResult = document.getElementById("card");
   albumResult.getElementsByTagName("p")[0].innerHTML = "hello";
+  // capture value from input field
+  const query = searchField.value;
 
-  // Cache the required nodes
-  const submit = document.querySelector(".submit");
-  const searchField = document.querySelector(".searchField");
+  // set any additional values for our query
+  const limit = "3";
+  const type = "album";
 
-  // Listen for a form submission
-  submit.addEventListener(".submit", function(e){
-    // prevent default
-    e.preventDefault();
+  // build query string with a temperate literal
+  //const api = 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=' + query + '&api_key='+ key + '&format=json';
 
-    // capture value from input field
-    const query = searchField.value;
+  albumResult.getElementsByTagName("h4")[0].innerHTML = responseObject.album.name;
+  albumLoad();
 
-    // set any additional values for our query
-    const limit = "3";
-    const type = "album";
+});
 
-    // build query string with a temperate literal
-    //const api = 'http://ws.audioscrobbler.com/2.0/?method=album.search&album=' + query + '&api_key='+ key + '&format=json';
+function albumLoad() {
+  //when response has loaded
+  xhr.onload = function() {
+    //create variable to store JSON data
+    var responseObject = JSON.parse(xhr.responseText);
 
-    albumResult.getElementsByTagName("h4")[0].innerHTML = responseObject.album.name;
-	});
-};
+    var albumResult = document.getElementById("card");
+    albumResult.getElementsByTagName("p")[0].innerHTML = "hello";
 
-xhr.open('GET', "http://ws.audioscrobbler.com/2.0/?method=album.search&album=mumford&api_key=99774229f0f8cf05d6ec0176a0109170&format=json", true);
-xhr.send(null);
+    // Cache the required nodes
+    const submit = document.querySelector(".submit");
+    const searchField = document.querySelector(".searchField");
+
+    xhr.open('GET', "http://ws.audioscrobbler.com/2.0/?method=album.search&album=" + query + "&api_key=99774229f0f8cf05d6ec0176a0109170&format=json", true);
+    xhr.send(null);
+
+  };
+}
